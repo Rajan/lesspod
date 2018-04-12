f031e911406e034ebaefd09b0aab8e076cc667b6
Rajan Chandi <chandi.rajan@gmail.com>
2018-04-12 08:22:09 +0530

    doT templating added

---
+++ js/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// import another component
+import main from './main.js';
+import doT from 'dot';
+main();
+function dotTest() {
+	var tempFn = doT.template("<h1>Here is a sample template {{=it.foo}}</h1>");
+	var resultText = tempFn({foo: 'with doT'});
+	console.log('resultText is ' + resultText);
+}
+window.onload = function(){dotTest();};
---
+++ js/main.js
@@ -0,0 +1,7 @@
+// import a CSS module
+import classes from '../css/main.css';
+// import fontawesome from '@fortawesome/fontawesome';
+
+export default () => {
+  console.log(classes.main);
+};
