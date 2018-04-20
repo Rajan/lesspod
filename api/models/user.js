9d96ac8b18caba892a7c206f4590dab9d36f5599
Rajan Chandi <chandi.rajan@gmail.com>
2018-04-20 10:54:36 +0530

    user registration working from register.ejs

--- api/models/user.js
+++ api/models/user.js
@@ -9,8 +9,7 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.INTEGER,
             allowNull: false,
             autoIncrement: true,
-            primaryKey: true,
-            defaultValue: 1
+            primaryKey: true
         },
         first     : DataTypes.STRING,
         last      : DataTypes.STRING,
--- index.js
+++ index.js
@@ -129,11 +129,11 @@ app.get('/posts', (req, res) => {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 
 // const config = require('parcel.config.js')
