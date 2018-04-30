d2b5b8f88bf7dd16273b21f0871a9a7493403b6f
Rajan Chandi <chandi.rajan@gmail.com>
2018-04-30 12:20:02 +0530

    getting all posts from api in Home.vue

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -54,13 +54,13 @@
 							</div>
 						</div>
 					</nav>
-					<div class="columns is-multiline">
-						<div class="column is-12-tablet is-6-desktop is-4-widescreen">
+					<div class="columns is-multiline" >
+						<div v-for="post of posts" class="column is-12-tablet is-6-desktop is-4-widescreen">
 							<article class="box">
 								<div class="media">
 									<div class="media-content">
 										<p class="title is-5 is-spaced is-marginless">
-											<a href="#">TensorFlow For Machine Intelligence</a>
+											<a href="#">{{post.title}}</a>
 										</p>
 										<div class="content is-small">
 											April 10, 2018
@@ -71,153 +71,206 @@
 											<p></p>
 										</div>
 									</div>
-								</div></article>
-							</div>
-
-							<div class="column is-12-tablet is-6-desktop is-4-widescreen">
-								<article class="box">
-									<div class="media">
-										<div class="media-content">
-											<p class="title is-5 is-marginless">
-												<a href="#">Docker in Production</a>
-											</p>
-											<div class="content is-small">
-												April 1, 2018
-												<br>
-												<a href="user.html">Edit</a>
-												<span>·</span>
-												<a>Delete</a>
-												<p></p>
-											</div>
-										</div>
-									</div></article>
 								</div>
-
-								<div class="column is-12-tablet is-6-desktop is-4-widescreen">
-									<article class="box">
-										<div class="media">
-											<div class="media-content">
-												<p class="title is-5 is-marginless">
-													<a href="#">Developing a Gulp.js Edge</a>
-												</p>
-												<div class="content is-small">
-													March 29, 2018
-													<br>
-													<a href="user.html">Edit</a>
-													<span>·</span>
-													<a>Delete</a>
-													<p></p>
-												</div>
-											</div>
-										</div></article>
-									</div>
-
-									<div class="column is-12-tablet is-6-desktop is-4-widescreen">
-										<article class="box">
-											<div class="media">
-												<div class="media-content">
-													<p class="title is-5 is-marginless">
-														<a href="#">Learning Swift</a>
-													</p>
-													<div class="content is-small">
-														Dec 25, 2017
-														<br>
-														<a href="user.html">Edit</a>
-														<span>·</span>
-														<a>Delete</a>
-														<p></p>
-													</div>
-												</div>
-											</div></article>
-										</div>
-
-										<div class="column is-12-tablet is-6-desktop is-4-widescreen">
-											<article class="box">
-												<div class="media">
-													<div class="media-content">
-														<p class="title is-5 is-marginless">
-															<a href="#">Choosing a JavaScript Framework</a>
-														</p>
-														<div class="content is-small">
-															Dec 5, 2017
-															<br>
-															<a href="user.html">Edit</a>
-															<span>·</span>
-															<a>Delete</a>
-															<p></p>
-														</div>
-													</div>
-												</div></article>
-											</div>
-
-											<div class="column is-12-tablet is-6-desktop is-4-widescreen">
-												<article class="box">
-													<div class="media">
-														<div class="media-content">
-															<p class="title is-5 is-marginless">
-																<a href="#">Deconstructing Google Cardboard Apps</a>
-															</p>
-															<div class="content is-small">
-																Nov 20, 2017
-																<br>
-																<a href="user.html">Edit</a>
-																<span>·</span>
-																<a>Delete</a>
-																<p></p>
-															</div>
-														</div>
-													</div></article>
-												</div>
-											</div>
-
-											<nav class="pagination">
-												<a class="pagination-previous">Previous</a>
-												<a class="pagination-next">Next page</a>
-												<ul class="pagination-list">
-													<li>
-														<a class="pagination-link">1</a>
-													</li>
-													<li>
-														<span class="pagination-ellipsis">&hellip;</span>
-													</li>
-													<li>
-														<a class="pagination-link">45</a>
-													</li>
-													<li>
-														<a class="pagination-link is-current">46</a>
-													</li>
-													<li>
-														<a class="pagination-link">47</a>
-													</li>
-													<li>
-														<span class="pagination-ellipsis">&hellip;</span>
-													</li>
-													<li>
-														<a class="pagination-link">86</a>
-													</li>
-												</ul>
-											</nav>
-										</div>
-									</div>
-								</div>
-							</div>
-	</section>
+							</article>
+							
+						<!-- <nav class="pagination">
+							<a class="pagination-previous">Previous</a>
+							<a class="pagination-next">Next page</a>
+							<ul class="pagination-list">
+								<li>
+									<a class="pagination-link">1</a>
+								</li>
+								<li>
+									<span class="pagination-ellipsis">&hellip;</span>
+								</li>
+								<li>
+									<a class="pagination-link">45</a>
+								</li>
+								<li>
+									<a class="pagination-link is-current">46</a>
+								</li>
+								<li>
+									<a class="pagination-link">47</a>
+								</li>
+								<li>
+									<span class="pagination-ellipsis">&hellip;</span>
+								</li>
+								<li>
+									<a class="pagination-link">86</a>
+								</li>
+							</ul>
+						</nav> -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
 </template>
 <script type="text/javascript">
 export default {
 	data(){
-		return {}
+		return {
+			posts: [
+			{
+				"id": 4,
+				"title": "Test Post",
+				"content": "Hello World!\nSome initial bold text\n\n",
+				"createdAt": "2018-04-23T15:12:08.584Z",
+				"updatedAt": "2018-04-23T15:12:08.584Z",
+				"Users": [
+				{
+					"id": 1,
+					"first": "Rajan",
+					"last": "GC",
+					"email": "chandi.rajan@gmail.com",
+					"phone": null,
+					"password": "$2b$10$3AeX3hbFZTmpHcHFECMNL.D65pxRYPjedsNYUc.J7h9m8j/NlSh0m",
+					"createdAt": "2018-04-20T09:17:42.303Z",
+					"updatedAt": "2018-04-20T09:17:42.303Z",
+					"UserPost": {
+						"id": 1,
+						"UserId": 1,
+						"PostId": 4,
+						"createdAt": "2018-04-23T15:12:08.602Z",
+						"updatedAt": "2018-04-23T15:12:08.602Z"
+					}
+				}
+				],
+				"UserPost": {
+					"id": 1,
+					"UserId": 1,
+					"PostId": 4,
+					"createdAt": "2018-04-23T15:12:08.602Z",
+					"updatedAt": "2018-04-23T15:12:08.602Z"
+				},
+				"users": [
+				{
+					"user": 1
+				}
+				]
+			},
+			{
+				"id": 6,
+				"title": "save post id",
+				"content": "Hello World!\nSome initial bold text\n\n",
+				"createdAt": "2018-04-23T17:57:06.619Z",
+				"updatedAt": "2018-04-23T17:57:06.619Z",
+				"Users": [
+				{
+					"id": 1,
+					"first": "Rajan",
+					"last": "GC",
+					"email": "chandi.rajan@gmail.com",
+					"phone": null,
+					"password": "$2b$10$3AeX3hbFZTmpHcHFECMNL.D65pxRYPjedsNYUc.J7h9m8j/NlSh0m",
+					"createdAt": "2018-04-20T09:17:42.303Z",
+					"updatedAt": "2018-04-20T09:17:42.303Z",
+					"UserPost": {
+						"id": 3,
+						"UserId": 1,
+						"PostId": 6,
+						"createdAt": "2018-04-23T17:57:06.629Z",
+						"updatedAt": "2018-04-23T17:57:06.629Z"
+					}
+				}
+				],
+				"UserPost": {
+					"id": 3,
+					"UserId": 1,
+					"PostId": 6,
+					"createdAt": "2018-04-23T17:57:06.629Z",
+					"updatedAt": "2018-04-23T17:57:06.629Z"
+				},
+				"users": [
+				{
+					"user": 1
+				}
+				]
+			},
+			{
+				"id": 7,
+				"title": "tagged post",
+				"content": "Hello World!\nSome initial bold text\n\n",
+				"createdAt": "2018-04-24T02:57:00.663Z",
+				"updatedAt": "2018-04-24T02:57:00.663Z",
+				"Users": [
+				{
+					"id": 1,
+					"first": "Rajan",
+					"last": "GC",
+					"email": "chandi.rajan@gmail.com",
+					"phone": null,
+					"password": "$2b$10$3AeX3hbFZTmpHcHFECMNL.D65pxRYPjedsNYUc.J7h9m8j/NlSh0m",
+					"createdAt": "2018-04-20T09:17:42.303Z",
+					"updatedAt": "2018-04-20T09:17:42.303Z",
+					"UserPost": {
+						"id": 4,
+						"UserId": 1,
+						"PostId": 7,
+						"createdAt": "2018-04-24T02:57:00.687Z",
+						"updatedAt": "2018-04-24T02:57:00.687Z"
+					}
+				}
+				],
+				"UserPost": {
+					"id": 4,
+					"UserId": 1,
+					"PostId": 7,
+					"createdAt": "2018-04-24T02:57:00.687Z",
+					"updatedAt": "2018-04-24T02:57:00.687Z"
+				},
+				"users": [
+				{
+					"user": 1
+				}
+				]
+			}
+			]
+
+
+		};
 	},
-	methods: {
-		newPost: function() {
-			console.log('new post');
-		},
-		logout: function() {
-			Cookies.set('token', '');
-			Cookies.set('user', '');
-			window.location.href = '../';
-		}
-	}
-}
-</script>
+	mounted: function() {
+					// Load all posts from the server and display them
+					// The view model.
+					var vm = this;
+					axios.defaults.headers.common['Authorization'] = Cookies.get("token");
+
+					axios.get('/v1/posts', {})
+					.then(function (response){
+						
+						// console.log(response);
+
+						let posts1 = response.data.posts;
+
+						for(var i in posts1){
+
+							console.log(posts[i].title);
+						}
+						this.$data.posts = posts1;
+						// renderPosts();
+					})
+					.catch(function (error){
+						console.log(error);
+						// if error is 401 unauthorize, logout the user.
+
+						if(error.toString().indexOf('401') !== -1){
+							logout();
+						}
+					});
+				},
+				methods: {
+					newPost: function() {
+						console.log('new post');
+					},
+					logout: function() {
+						Cookies.set('token', '');
+						Cookies.set('user', '');
+						window.location.href = '../';
+					}
+				}
+			}
+			</script>
