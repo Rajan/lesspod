4b31c8ab2cf1de4a3d1785485a6302f257bb3479
Sujay Kundu <sujaykundu777@gmail.com>
2018-05-12 03:30:01 +0530

    importing bulma

---
+++ client/react/src/components/common/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Lesspod </h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- client/react/src/index.js
+++ client/react/src/index.js
@@ -7,9 +7,9 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    const HApp = require('./App').default;
-    ReactDOM.render(<HApp />, document.getElementById('root'));
-  });
-}
+// if (module.hot) {
+//   module.hot.accept('./App', () => {
+//     const HApp = require('./App').default;
+//     ReactDOM.render(<HApp />, document.getElementById('root'));
+//   });
+// }
