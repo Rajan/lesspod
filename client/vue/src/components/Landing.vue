8208e1f7eb30fc51a7d73bcf7365dbe7d885e348
madguy02 <manishkakoti688@gmail.com>
2018-05-13 20:47:35 +0530

    updated landing-page

--- client/vue/src/components/Landing.vue
+++ client/vue/src/components/Landing.vue
@@ -1,5 +1,5 @@
 <template>
-<h1>Landing page coming soon</h1>
+<img src = "../assets/images/landing.jpg">
 </template>
 <script type="text/javascript">
 module.exports = {
@@ -9,10 +9,16 @@ module.exports = {
 };
 </script>
 
-<style media="screen">
+<style media="screen" scoped>
 	h1 {
 		text-align: center;
 		font-size: 30px;
-		
+
+	}
+	img {
+		height: 600px;
+		width: 100%;
+		border-radius: 10px;
+		border-left: 10px
 	}
 </style>
