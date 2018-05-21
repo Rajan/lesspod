a26ae0777c58dd695ac9496b7f0ea148fedb5f79
Rajan Chandi <chandi.rajan@gmail.com>
2018-05-21 19:44:00 +0530

    basic landing page

--- client/vue/src/components/Landing.vue
+++ client/vue/src/components/Landing.vue
@@ -1,14 +1,50 @@
 <template>
-<section class="section">
-	<div class="container">
-		<div class="columns is-centered is-multiline">
-			<div class="column is-two-thirds">
-				This is a landing page
-				<a href="/login">Login</a> Or <a href="/register">Create Account</a>
+	<section class="section">
+
+		<div class="hero-body">
+			<div class="container has-text-centered">
+				<div class="columns is-vcentered">
+					<div class="column is-5">
+						<figure class="image is-4by3">
+							<img src="./../assets/images/serverless.png" alt="Description">
+						</figure>
+					</div>
+					<div class="column is-6 is-offset-1">
+						<h1 class="title is-2">
+							Serverless Blogging Engine
+						</h1>
+						<h2 class="subtitle is-4"><br>
+							Lesspod lets build and deploy personal website+blog to Firebase. Key benefits are listed below.
+						</h2>
+						<!-- <div class="content"> -->
+							<ul style="font-size: 1.3rem;">
+								<li>No fixed recurring hosting fees.</li>
+								<li>Infinite scalability of the cloud.</li>
+								<li>Free hosting till you're very popular.</li>
+							</ul>
+						<!-- </div> -->
+						<br>
+						<p class="has-text-centered">
+							<a href="https://github.com/Rajan/lesspod" target="_blank" class="button is-medium is-success is-outlined">
+								Lesspod on Github
+							</a>
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
+
+		<div class="hero-foot">
+			<div class="container">
+				<div class="is-centered">
+					<div class="column has-text-centered" style="font-size:1.5rem;">
+						This is a landing page
+						<a href="/login">Login</a> Or <a href="/register">Create Account</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 <script type="text/javascript">
 module.exports = {
