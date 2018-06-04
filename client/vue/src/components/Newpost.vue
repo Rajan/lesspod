3d71dfd9cea99c41b175e63801f8b253290e99b3
s-kris <skris4291@gmail.com>
2018-06-03 23:44:52 -0400

    fbase added for Newpost.vue

--- client/vue/src/components/Newpost.vue
+++ client/vue/src/components/Newpost.vue
@@ -1,188 +1,223 @@
 <template>
-	<section class="section">
-		<div class="container">
-			<div class="columns is-centered is-multiline has-text-centered">
-				<div class="column is-two-thirds">
-					<div class="field is-horizontal">
-						<div class="field-body">
-							<div class="field">
-								<p class="control">
-									<input class="input has-text-centered is-medium" id="title" type="text" placeholder="Post Title" style="font-weight: bold;font-size:2rem;" >
-								</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="column is-two-thirds">
+<section class="section">
+  <div class="container">
+    <div class="columns is-centered is-multiline has-text-centered">
+      <div class="column is-two-thirds">
+        <div class="field is-horizontal">
+          <div class="field-body">
+            <div class="field">
+              <p class="control">
+                <input class="input has-text-centered is-medium" id="title" type="text" placeholder="Post Title" style="font-weight: bold;font-size:2rem;">
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column is-two-thirds">
 
-				<quill-editor v-model="editor"
-					:options="editorOption" style="height: 20rem;">
-				</quill-editor>
+        <quill-editor v-model="editor" :options="editorOption" style="height: 20rem;">
+        </quill-editor>
 
-				<br><br><br>
+        <br><br><br>
 
-				<a href="#" class="button is-primary" @click="savePost">
+        <a href="#" class="button is-primary" @click="savePost">
 					Save Post
 				</a><br><br><br>
-				<!-- <input-tag :tags.sync="tagsArray" placeholder="Add Tag"></input-tag> -->
-				<br><br>
-				<input type="hidden" name="postId" id="postId" value="" />
-			</div>
+        <!-- <input-tag :tags.sync="tagsArray" placeholder="Add Tag"></input-tag> -->
+        <br><br>
+        <input type="hidden" name="postId" id="postId" value="" />
+      </div>
 
-		</div>
-	</div>
-</div>
+    </div>
+  </div>
+  </div>
 </section>
 </template>
 
 <script type="text/javascript">
-import { globalVariables } from './../main';
+import {
+  globalVariables
+} from './../main';
 export default {
-	data(){
-		return {
-			editor: '',
-			editorOption: {
-				modules: {
-					toolbar: [
-					[{ 'size': ['small', false, 'large'] }],
-					['bold', 'italic'],
-					[{ 'list': 'ordered'}, { 'list': 'bullet' }],
-					['link', 'image', 'video']
-					],
-					history: {
-						delay: 1000,
-						maxStack: 50,
-						userOnly: false
-					},
-					imageDrop: true,
-					 imageResize: {
-					 	displayStyles: {
-					 		backgroundColor: 'black',
-					 		border: 'none',
-					 		color: 'white'
-					 	},
-					 	modules: [ 'Resize', 'DisplaySize', 'Toolbar' ]
-					 }
-				}
-			},
-			tagsArray: []
-		}
-	},
-	beforeMount() {
-		this.initPost();
-	},
-	computed: {
-		contentCode() {
-			return hljs.highlightAuto(this.content).value;
-		}
-	},
-	methods: {
-		initPost: function() {
+  data() {
+    return {
+      editor: '',
+      editorOption: {
+        modules: {
+          toolbar: [
+            [{
+              'size': ['small', false, 'large']
+            }],
+            ['bold', 'italic'],
+            [{
+              'list': 'ordered'
+            }, {
+              'list': 'bullet'
+            }],
+            ['link', 'image', 'video']
+          ],
+          history: {
+            delay: 1000,
+            maxStack: 50,
+            userOnly: false
+          },
+          imageDrop: true,
+          imageResize: {
+            displayStyles: {
+              backgroundColor: 'black',
+              border: 'none',
+              color: 'white'
+            },
+            modules: ['Resize', 'DisplaySize', 'Toolbar']
+          }
+        }
+      },
+      tagsArray: []
+    }
+  },
+  beforeMount() {
+    this.initPost();
+  },
+  computed: {
+    contentCode() {
+      return hljs.highlightAuto(this.content)
+        .value;
+    }
+  },
+  methods: {
+    initPost: function() {
 
-			axios.defaults.headers.common['Authorization'] = Cookies.get("token");
+      axios.defaults.headers.common['Authorization'] = Cookies.get("token");
 
-		},
-		savePost: function() {
-			console.log('saving a post...');
-			var vm = this;
-			var title = document.getElementById("title").value;
-			var content = this.editor;
-			console.log('title is ' + title.toString() + ' content is ' + content.toString());
-			if(title.length && content.length) {
+    },
+    savePost: function() {
+      console.log('saving a post...');
+      var vm = this;
+      var title = document.getElementById("title")
+        .value;
+      var content = this.editor;
+      console.log('title is ' + title.toString() + ' content is ' + content.toString());
+      if (title.length && content.length) {
 
-				const { deploymentTarget, LOCALHOST, FBASE } = globalVariables;
-				console.log('deployment target is ' + deploymentTarget);
+        const {
+          deploymentTarget,
+          LOCALHOST,
+          FBASE
+        } = globalVariables;
+        console.log('deployment target is ' + deploymentTarget);
 
-				const postData = {
-					"title" : title.toString(),
-					"content" : content.toString(),
-					"tags" : this.tagsArray.toString()
-				};
+        const postData = {
+          "title": title.toString(),
+          "content": content.toString(),
+          "tags": this.tagsArray.toString()
+        };
 
-				switch (deploymentTarget) {
-					case LOCALHOST:
-					axios.post('/v1/posts', postData)
-					.then(function (response) {
-						console.log(response);
-						console.log('Post Id is ' + response.data.post.id.toString());
-						document.getElementById('postId').value = response.data.post.id.toString();
-						Cookies.set("post", response.data.post);
-						// this.$notify('Post saved successfully!', 'success');
-						window.location.href = '../home'
-					})
-					.catch(function (error) {
-						console.log(error);
-						if(error.toString().indexOf('401') !== 0){
-							vm.logout();
-						}
+        switch (deploymentTarget) {
+          case LOCALHOST:
+            axios.post('/v1/posts', postData)
+              .then(function(response) {
+                console.log(response);
+                console.log('Post Id is ' + response.data.post.id.toString());
+                document.getElementById('postId')
+                  .value = response.data.post.id.toString();
+                Cookies.set("post", response.data.post);
+                // this.$notify('Post saved successfully!', 'success');
+                window.location.href = '../home'
+              })
+              .catch(function(error) {
+                console.log(error);
+                if (error.toString().indexOf('401') !== 0) {
+                  vm.logout();
+                }
 
-					});
-					break;
+              });
+            break;
 
-					case FBASE:
-					var db = firebase.firestore();
-					const settings = { timestampsInSnapshots: true };
+          case FBASE:
+            var db = firebase.firestore();
+            const settings = {
+              timestampsInSnapshots: true
+            };
+            db.settings(settings);
 
-					db.settings(settings);
-					db.collection("posts").add(postData)
-					.then(function(docRef) {
-						console.log("Document written with ID: ", docRef.id);
-					})
-					.catch(function(error) {
-						console.error("Error adding document: ", error);
-					});
-					break;
-				}
-			} else {
-				console.log('nothing to save...');
-			}
-		},
-		logout: function() {
-			Cookies.set('token', '');
-			Cookies.set('user', '');
-			window.location.href = '../';
-		},
-		onEditorBlur(editor) {
-        	// console.log('editor blur!', editor)
-   		},
-    	onEditorFocus(editor) {
-	        // console.log('editor focus!', editor)
-	    },
-	    onEditorReady(editor) {
-	        // console.log('editor ready!', editor)
-	    },
-	    addTag: function() {
-	    	console.log('adding a tag...');
+            postData.createdBy(Cookies.get('user').id);
+            db.collection("posts")
+              .add(postData)
+              .then(function(docRef) {
+                console.log("Post: Document written with ID: ", docRef.id);
+                Cookies.set("post", postData);
+                // this.$notify('Post saved successfully!', 'success');
+                window.location.href = '../home'
 
-	    	let tagText = document.getElementById("tag").value;
-	    	let user = Cookies.getJSON("user");
+              })
+              .catch(function(error) {
+                console.error("Error adding document: ", error);
+                if (error.toString().indexOf('401') !== 0) {
+                  firebase.auth().signOut().then(function() {
+                    // Sign-out successful.
+                  }, function(error) {
+                    // An error happened.
+                  });
+                }
+              });
+            break;
+        }
+      } else {
+        console.log('nothing to save...');
+      }
+    },
+    logout: function() {
+      Cookies.set('token', '');
+      Cookies.set('user', '');
+      window.location.href = '../';
+    },
+    onEditorBlur(editor) {
+      // console.log('editor blur!', editor)
+    },
+    onEditorFocus(editor) {
+      // console.log('editor focus!', editor)
+    },
+    onEditorReady(editor) {
+      // console.log('editor ready!', editor)
+    },
+    addTag: function() {
+      console.log('adding a tag...');
 
-	    	if(tagText.length && document.getElementById("postId").value.length) {
-					// axios.defaults.headers.common['Authorization'] = Cookies.get("token");
-					axios.post('/v1/tags', {
+      let tagText = document.getElementById("tag")
+        .value;
+      let user = Cookies.getJSON("user");
 
-						"name" : tagText,
-						"postId" : document.getElementById('postId').value,
-						"userId" : user.id.toString()
+      if (tagText.length && document.getElementById("postId")
+        .value.length) {
+        // axios.defaults.headers.common['Authorization'] = Cookies.get("token");
+        axios.post('/v1/tags', {
 
-					})
-					.then(function (response) {
-						console.log(response);
-						if(response.data.success) {
-							document.getElementById("tag").value = '';
-							console.log('Tag: ' + response.data.tag.name.toString() + ' added successfully.');
-						}
-					})
-					.catch(function (error) {
-						console.log(error);
-					});
-			}
-		}
-	}
+            "name": tagText,
+            "postId": document.getElementById('postId')
+              .value,
+            "userId": user.id.toString()
+
+          })
+          .then(function(response) {
+            console.log(response);
+            if (response.data.success) {
+              document.getElementById("tag")
+                .value = '';
+              console.log('Tag: ' + response.data.tag.name.toString() + ' added successfully.');
+            }
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      }
+    }
+  }
 };
 </script>
 <style>
-	section, body, html {
-		background: white !important;
-	}
+section,
+body,
+html {
+  background: white !important;
+}
 </style>
--- client/vue/src/main.js
+++ client/vue/src/main.js
@@ -1,5 +1,5 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// The Vue build version to load with the `import` command (runtime-only or
+// standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
 import router from './router';
@@ -11,79 +11,89 @@ import Notify from 'vue2-notify';
 import hljs from 'highlight.js';
 import VueQuillEditor, { Quill } from 'vue-quill-editor';
 import { ImageDrop } from 'quill-image-drop-module';
- //import ImageResize from 'quill-image-resize-module';
+//import ImageResize from 'quill-image-resize-module';
 
- import VueQuill1 from 'vue-quill-editor'
- window.Quill = Quill;
+import VueQuill1 from 'vue-quill-editor'
+window.Quill = Quill;
 
- const ImageResize = require( 'quill-image-resize-module' );
+const ImageResize = require( 'quill-image-resize-module' );
 
- Quill.register('modules/imageResize', ImageResize);
+Quill.register( 'modules/imageResize', ImageResize );
 // require styles
- import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
- import 'quill/dist/quill.bubble.css'
+import 'quill/dist/quill.bubble.css'
 
 import VueDisqus from 'vue-disqus'
 
 //////////////////////////////
-const toolbar = [['image']];
-const modules = { toolbar, imageResize: true };
-Vue.use(VueQuill1,{modules});
+const toolbar = [ [ 'image' ] ];
+const modules = {
+  toolbar,
+  imageResize: true
+};
+Vue.use( VueQuill1, { modules } );
 ////////////////////////////////////
 
-Vue.use(VueQuillEditor, /* { default global options } */)
+Vue.use( VueQuillEditor,
+/* { default global options } */ )
 
-Quill.register('modules/imageDrop', ImageDrop);
+Quill.register( 'modules/imageDrop', ImageDrop );
 
-Vue.use(VueQuill);
+Vue.use( VueQuill );
 
-Vue.use(VModal);
+Vue.use( VModal );
 
-Vue.use(VueDisqus);
+Vue.use( VueDisqus );
 
-Vue.use(Notify, {
+Vue.use( Notify, {
   itemClass: 'notification',
   position: 'bottom-left'
-})
+} )
 const types = {
-  info: { itemClass: 'is-info', },
-  error: { itemClass: 'is-danger' },
-  warning: { itemClass: 'is-warning' },
-  success: { itemClass: 'is-success', iconClass: 'fa fa-lg fa-check-circle' }
+  info: {
+    itemClass: 'is-info'
+  },
+  error: {
+    itemClass: 'is-danger'
+  },
+  warning: {
+    itemClass: 'is-warning'
+  },
+  success: {
+    itemClass: 'is-success',
+    iconClass: 'fa fa-lg fa-check-circle'
+  }
 }
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyD_9U3AZqZ-cz1jr2ZK3TW4DCyyshoiXF4",
-    authDomain: "lesspod-dev.firebaseapp.com",
-    databaseURL: "https://lesspod-dev.firebaseio.com",
-    projectId: "lesspod-dev",
-    storageBucket: "",
-    messagingSenderId: "406114890288"
+  apiKey: "AIzaSyD_9U3AZqZ-cz1jr2ZK3TW4DCyyshoiXF4",
+  authDomain: "lesspod-dev.firebaseapp.com",
+  databaseURL: "https://lesspod-dev.firebaseio.com",
+  projectId: "lesspod-dev",
+  storageBucket: "",
+  messagingSenderId: "406114890288"
 };
-firebase.initializeApp(config);
+firebase.initializeApp( config );
 
-// db calls are made based on this deploymentTarget's value
-// fbase deploy script will change it's value
-export const globalVariables = new Vue({
+// db calls are made based on this deploymentTarget's value fbase deploy script
+// will change it's value
+export const globalVariables = new Vue( {
   data: {
     deploymentTarget: 'localhost',
     LOCALHOST: 'localhost',
     FBASE: 'firebase'
   }
-})
+} )
 
-Vue.$notify.setTypes(types);
+Vue.$notify.setTypes( types );
 
-Vue.component('input-tag', InputTag);
+Vue.component( 'input-tag', InputTag );
 
 Vue.config.productionTip = false
 axios.defaults.baseURL = 'http://localhost:1234/';
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-});
+new Vue( { el: '#app', router, components: {
+    App
+  }, template: '<App/>' } );
