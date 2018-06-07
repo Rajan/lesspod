60c280e01ff77db71f25dce97013e3d2e3d92ef2
Rajan Chandi <chandi.rajan@gmail.com>
2018-06-07 11:09:43 +0530

    minor ui fixes

--- client/vue/src/components/Viewpost.vue
+++ client/vue/src/components/Viewpost.vue
@@ -57,12 +57,12 @@
   </div>
   </div>
   <div class="icon-bar">
-    <a v-bind:href="fbUrl" class="fbcalm"><i class="fab fa-facebook-f"></i></a>
+    <a v-bind:href="fbUrl" class="calm"><i class="fab fa-facebook-f"></i></a>
     <a v-bind:href="twitterUrl" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;" target="_blank" title="Share on Twitter"
-      class="twcalm"><i class="fab fa-twitter"></i>
+      class="calm"><i class="fab fa-twitter"></i>
 		</a>
     <!-- <a href="#" class="google"><i class="fab fa-google"></i></a>  -->
-    <a v-bind:href="linkedinUrl" class="licalm"><i class="fab fa-linkedin"></i></a>
+    <a v-bind:href="linkedinUrl" class="calm"><i class="fab fa-linkedin"></i></a>
     <!-- <a href="#" class="youtube"><i class="fab fa-youtube"></i></a>  -->
   </div>
 </section>
@@ -279,7 +279,7 @@ body {
  	.icon-bar {
   		right: 0%;
   		left: auto;
-  		top:81%;
+  		top:78%;
 	}
 
   #footer {
