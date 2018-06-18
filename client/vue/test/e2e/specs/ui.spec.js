e8b7cdb82fe729767423f6d1b7c3382fd2b2c6e2
vk92kokil <vk92kokil@gmail.com>
2018-06-18 13:43:20 +0530

    added settings and profile test cases

--- client/vue/test/e2e/specs/ui.spec.js
+++ client/vue/test/e2e/specs/ui.spec.js
@@ -8,7 +8,8 @@ const DUMMY_USER = {
   first: faker.name.firstName(),
   last: faker.name.lastName(),
   email: faker.internet.email(),
-  password: faker.internet.password()
+  password: faker.internet.password(),
+  newPassword: faker.internet.password()
 }
 module.exports = {
   after: function (browser, done) {
@@ -71,7 +72,7 @@ module.exports = {
       })
       .pause(2000)
   },
-  'Post e2e': function (browser) {
+  'Post CREATE': function (browser) {
     browser
       .click('p a[href="../newpost"]')
       .pause(10000)
@@ -81,9 +82,37 @@ module.exports = {
       .setValue('div[contenteditable=true]', faker.lorem.text())
       .pause(2000)
       .click('a[class="button is-primary"]')
+      .pause(5000)
+  },
+  'Post VIEW': function (browser) {
+    browser
+      .click('div[class="media"] div[class="content "] a:nth-child(6)')
+      .pause(1000)
+      .assert.urlContains('/post')
+      .assert.elementPresent('input[id="title"][readonly="readonly"]')
+      .pause(2000)
+      .back()
       .pause(2000)
   },
-  'Menu e2e': function (browser) {
+  'Post UPDATE': function (browser) {
+    browser
+      .click('div[class="media"] div[class="content "] a:nth-child(4)')
+      .pause(1000)
+      .assert.urlContains('/editpost')
+      .setValue('input[id="title"]', ' ' + faker.random.word())
+      .setValue('div[contenteditable=true]', ' ' + faker.lorem.text())
+      .pause(2000)
+      .click('a[class="button is-primary"]')
+      .pause(5000)
+  },
+  'Post DELETE': function (browser) {
+    browser
+      .assert.urlContains('/home')
+      .pause(10000)
+      .click('div[class="media"] div[class="content "] a:nth-child(8)')
+      .pause(2000)
+  },
+  'Menu CREATE': function (browser) {
     browser
       .moveToElement('#create-new', 10, 10)
       .click('#new-menu')
@@ -93,5 +122,31 @@ module.exports = {
       .pause(2000)
       .click('button[class="button is-success is-small"]')
       .pause(2000)
+  },
+  'Menu DELETE': function (browser) {
+    browser
+      .click('button a[class="tag is-danger is-delete"]')
+      .pause(10000)
+  },
+  'Profile UPDATE': function (browser) {
+    browser
+      .moveToElement('#profile', 10, 10)
+      .click('a[href="/profile"]')
+      .pause(2000)
+      .setValue('input[id="fullName"]', faker.name.firstName() + ' ' + faker.name.lastName())
+      .setValue('input[id="email"]', faker.internet.email())
+      .setValue('input[id="password"]', DUMMY_USER.newPassword)
+      .setValue('input[id="password-confirm"]', DUMMY_USER.newPassword)
+      .pause(2000)
+      .click('button[class="button is-info"]')
+  },
+  'Settings Update': function (browser) {
+    browser
+      .moveToElement('#profile', 10, 10)
+      .click('a[href="/settings"]')
+      .pause(2000)
+      .setValue('input[id="tagline"]', faker.lorem.sentence())
+      .pause(2000)
+      .click('button[class="button is-info"]')
   }
 }
