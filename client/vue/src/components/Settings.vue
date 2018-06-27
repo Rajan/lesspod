b2c005f5e7f1c226c3aa57387a40bd543e9d9508
vk92kokil <vk92kokil@gmail.com>
2018-06-27 14:42:53 +0530

    settings page update

--- client/vue/src/components/Settings.vue
+++ client/vue/src/components/Settings.vue
@@ -1,22 +1,22 @@
 <template>
-<section class="hero is-info">
-  <div class="hero-body">
-    <div class="container">
-      <div class="columns is-centered">
-        <div class="column is-5-tablet is-4-desktop is-4-widescreen">
-          <form class="box" onsubmit="event.preventDefault();">
-            <div class="field has-text-centered">
+  <section class="hero is-info">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-5-tablet is-4-desktop is-4-widescreen">
+            <form class="box">
+              <div class="field has-text-centered">
               <span class="icon" style="width: 3rem; height: 3rem;">
 									<i class="fa fa-cog fas fa-3x"></i>
 								</span><br> Site Settings
-            </div>
-            <div class="field">
-              <label class="label">Square Logo</label>
-              <div class="control has-icons-left">
-                <div class="file has-name is-boxed">
-                  <label class="file-label">
+              </div>
+              <div class="field">
+                <label class="label">Square Logo</label>
+                <div class="control has-icons-left">
+                  <div class="file has-name is-boxed">
+                    <label class="file-label">
                       <input class="file-input" type="button" name="resume"
-                             @click.stop="updateImage('squareLogo')">
+                            @click.stop="updateImage('squareLogo')">
                       <span class="file-cta">
 												<span class="file-icon">
 													<i class="fas fa-upload"></i>
@@ -29,15 +29,15 @@
 												{{sqLogo}}
 											</span>-->
                     </label>
+                  </div>
                 </div>
               </div>
-            </div>
-            <br>
-            <div class="field">
-              <label class="label">Horizontal Logo</label>
-              <div class="control has-icons-left">
-                <div class="file has-name is-boxed">
-                  <label class="file-label">
+              <br>
+              <div class="field">
+                <label class="label">Horizontal Logo</label>
+                <div class="control has-icons-left">
+                  <div class="file has-name is-boxed">
+                    <label class="file-label">
                       <input class="file-input" type="button" name="resume"
                              @click.stop="updateImage('horizontalLogo')">
                       <span class="file-cta">
@@ -52,6 +52,7 @@
 												{{rectLogo}}
 											</span>-->
                     </label>
+                  </div>
                 </div>
               </div>
               <div class="field">
@@ -61,53 +62,50 @@
                          placeholder="e.g. Serverless Blogging Engine" autocomplete="name" required>
                 </div>
               </div>
-            </div>
-            <div class="field">
-              <label class="label">Blog Menu</label>
-              <div class="control has-icons-left">
-                <div class="control">
-                  <label class="checkbox">
+              <div class="field">
+                <label class="label">Blog Menu</label>
+                <div class="control has-icons-left">
+                  <div class="control">
+                    <label class="checkbox">
                       <input type="checkbox" v-model="disableBlog">
                       Disable Blog Menu
                     </label>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="field">
-              <label class="label">New Registrations?</label>
-              <div class="control has-icons-left">
-                <div class="control">
-                  <label class="checkbox">
+              <div class="field">
+                <label class="label">New Registrations?</label>
+                <div class="control has-icons-left">
+                  <div class="control">
+                    <label class="checkbox">
                       <input type="checkbox" v-model="disableSignups">
                       Disable New Registrations
                     </label>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="field is-grouped" style="margin-top: 1.5rem;">
-              <div class="control">
-                <button class="button is-info" @click="saveSettings">Save Settings</button>
+              <div class="field is-grouped" style="margin-top: 1.5rem;">
+                <div class="control">
+                  <button class="button is-info" @click="saveSettings">Save Settings</button>
+                </div>
+                <div class="control">
+                  <a class="button is-text" style="text-decoration: none;" href="home">Cancel</a>
+                </div>
               </div>
-              <div class="control">
-                <a class="button is-text" style="text-decoration: none;" href="home">Cancel</a>
-              </div>
-            </div>
-            <!-- <div class="field">
+              <!-- <div class="field">
                     <button class="button is-success">
                       Login
                     </button>
                 </div> -->
-          </form>
+            </form>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 </template>
 <script type="text/javascript">
-import firebase from "firebase";
 import { globalVariables } from "./../main";
-
 export default {
   data() {
     return {
@@ -123,37 +121,30 @@ export default {
     const vm = this;
     const { deploymentTarget, LOCALHOST, FBASE } = globalVariables;
     console.log("deployment target is " + deploymentTarget);
-
     this.$root.$upload.new("squareLogo", {
       url: "v1/settings/logo",
       name: "logo",
-      body: {
-        logoType: "squareLogo"
-      },
+      body: { logoType: "squareLogo" },
       onSuccess: res => {
         console.log("Logo uploaded Successfully");
         location.reload();
       },
       onError(error) {
-        alert("Unable to upload profile pic");
+        alert("Unable to upload squareLogo pic");
       }
     });
-
     this.$root.$upload.new("horizontalLogo", {
       url: "v1/settings/logo",
       name: "logo",
-      body: {
-        logoType: "horizontalLogo"
-      },
+      body: { logoType: "horizontalLogo" },
       onSuccess: res => {
         console.log("Logo uploaded Successfully");
         location.reload();
       },
       onError(error) {
-        alert("Unable to upload profile pic");
+        alert("Unable to upload horizontalLogo pic");
       }
     });
-
     switch (deploymentTarget) {
       case LOCALHOST:
         axios
@@ -177,7 +168,6 @@ export default {
           .catch(function(error) {
             console.log(error);
             // if error is 401 unauthorize, logout the user.
-
             if (error.toString().indexOf("401") !== -1) {
               console.log("Logging you out...");
               vm.logout();
@@ -190,94 +180,57 @@ export default {
           timestampsInSnapshots: true
         };
         db.settings(dbSettings);
-
         const user = this.$cookie.getJSON("user");
-
-        var docRef = db.collection("settings").doc(user.id);
-
-        docRef
+        db
+          .collection("settings")
+          .where("createdBy", "==", user.id)
           .get()
-          .then(function(doc) {
-            if (doc.exists) {
-              const settingsData = doc.data();
-              for (let key in settingsData) {
-                vm[key] = settingsData[key];
+          .then(function(querySnapshot) {
+            let settings = [];
+            querySnapshot.forEach(function(doc) {
+              settings.push(doc.data());
+            });
+            for (let i in settings) {
+              let setting = settings[i];
+              switch (setting.name) {
+                case "disableBlog":
+                case "disableSignups":
+                  vm[setting.name] = setting.value === "1";
+                  break;
+                default:
+                  vm[setting.name] = setting.value;
               }
-            } else {
-              // doc.data() will be undefined in this case
-              console.log("No user settings data yet!");
             }
           })
           .catch(function(error) {
-            console.log("Error getting document:", error);
+            console.log("Error getting settings: ", error);
           });
-
         break;
     }
   },
   methods: {
-    updateLogoPic: function(logoType) {
-      // updating of square of horizontal logo
-      const { deploymentTarget, LOCALHOST, FBASE } = globalVariables;
-      switch (deploymentTarget) {
-        case LOCALHOST:
-          this.$root.$upload.select(logoType);
-          break;
-
-        case FBASE:
-          // Firebase code to upload pic.
-
-          break;
-      }
+    updateImage: function(logoType) {
+      this.$root.$upload.select(logoType);
     },
     saveSettings: function() {
       const vm = this;
       let settings = vm.settings;
-
       const { deploymentTarget, LOCALHOST, FBASE } = globalVariables;
       console.log("deployment target is " + deploymentTarget);
-
-      this.$root.$upload.new("squareLogo", {
-        url: "v1/settings/logo",
-        name: "logo",
-        body: { logoType: "squareLogo" },
-        onSuccess: res => {
-          console.log("Logo uploaded Successfully");
-          location.reload();
-        },
-        onError(error) {
-          alert("Unable to upload squareLogo pic");
-        }
-      });
-
-      this.$root.$upload.new("horizontalLogo", {
-        url: "v1/settings/logo",
-        name: "logo",
-        body: { logoType: "horizontalLogo" },
-        onSuccess: res => {
-          console.log("Logo uploaded Successfully");
-          location.reload();
-        },
-        onError(error) {
-          alert("Unable to upload horizontalLogo pic");
-        }
-      });
-
-      switch (deploymentTarget) {
-        case LOCALHOST:
-          // updating existing settings
-          for (let i in settings) {
-            let setting = settings[i];
-            // switch(setting.name){
-            // case "tagline":
-            let settingData = {
-              id: setting.id,
-              name: setting.name,
-              // value: vm.tagline
-              value: vm[setting.name]
-            };
-
-            if (setting.name) {
+      // updating existing settings
+      for (let i in settings) {
+        let setting = settings[i];
+        // switch(setting.name){
+        // case "tagline":
+        let settingData = {
+          id: setting.id,
+          name: setting.name,
+          // value: vm.tagline
+          value: vm[setting.name]
+        };
+        if (setting.name) {
+          switch (deploymentTarget) {
+            case LOCALHOST:
               axios
                 .put("/v1/settings/" + setting.id, settingData)
                 .then(function(response) {
@@ -286,20 +239,42 @@ export default {
                 .catch(function(error) {
                   console.log(error);
                 });
-            }
-            // break;
-
-            // }
-          }
-
-          for (let settingKey of vm.settingName) {
-            const found = vm.settings.find(f => settingKey === f.name);
-            if (!found && vm[settingKey]) {
-              let settingData = {
-                name: settingKey,
-                value: vm[settingKey]
+              break;
+            case FBASE:
+              let db = firebase.firestore();
+              const dbSettings = {
+                timestampsInSnapshots: true
               };
-
+              const moment = require("moment");
+              settingData.updatedAt = moment().format(
+                "YYYY-MM-DD HH:mm:ss.ms Z"
+              );
+              db.settings(dbSettings);
+              db
+                .collection("settings")
+                .doc(setting.id)
+                .update(settingData)
+                .then(function(docRef) {
+                  console.log("settings saved!");
+                })
+                .catch(function(error) {
+                  console.error("Error adding document: ", error);
+                });
+              break;
+          }
+        }
+        // break;
+        // }
+      }
+      for (let settingKey of vm.settingName) {
+        const found = vm.settings.find(f => settingKey === f.name);
+        if (!found && vm[settingKey]) {
+          let settingData = {
+            name: settingKey,
+            value: vm[settingKey]
+          };
+          switch (deploymentTarget) {
+            case LOCALHOST:
               axios
                 .post("/v1/settings/", settingData)
                 .then(function(response) {
@@ -308,131 +283,63 @@ export default {
                 .catch(function(error) {
                   console.log(error);
                 });
-            }
+              break;
+            case FBASE:
+              let db = firebase.firestore();
+              const dbSettings = {
+                timestampsInSnapshots: true
+              };
+              db.settings(dbSettings);
+              const uuidv4 = require("uuid/v4");
+              settingData.id = uuidv4();
+              settingData.createdBy = vm.$cookie.getJSON("user").id;
+              const moment = require("moment");
+              settingData.createdAt = moment().format(
+                "YYYY-MM-DD HH:mm:ss.ms Z"
+              );
+              settingData.updatedAt = moment().format(
+                "YYYY-MM-DD HH:mm:ss.ms Z"
+              );
+              db
+                .collection("settings")
+                .doc(setting.id)
+                .set(settingData)
+                .then(function(docRef) {
+                  console.log("settings added!");
+                })
+                .catch(function(error) {
+                  console.error("Error adding document: ", error);
+                });
+              break;
           }
-          break;
-        case FBASE:
-          let settingsData = {};
-          for (let key of vm.settingName) {
-            settingsData[key] = vm[key];
-          }
-
-          let db = firebase.firestore();
-          const dbSettings = {
-            timestampsInSnapshots: true
-          };
-          db.settings(dbSettings);
-
-          const user = vm.$cookie.getJSON("user");
-
-          settingsData.createdBy = user.id;
-
-          const moment = require("moment");
-          settingsData.updatedAt = moment().format("YYYY-MM-DD HH:mm:ss.ms Z");
-
-          db
-            .collection("settings")
-            .doc(user.id)
-            .set(settingsData)
-            .then(function(docRef) {
-              console.log("settings added!");
-              window.location.reload();
-            })
-            .catch(function(error) {
-              console.error("Error adding document: ", error);
-            });
-          break;
-      }
-    },
-    methods: {
-      updateImage: function(logoType) {
-        this.$root.$upload.select(logoType);
-      },
-      saveSettings: function() {
-        const vm = this;
-        let settings = vm.settings;
-
-        const { deploymentTarget, LOCALHOST, FBASE } = globalVariables;
-        console.log("deployment target is " + deploymentTarget);
-
-        // updating existing settings
-        for (let i in settings) {
-          let setting = settings[i];
-          // switch(setting.name){
-          // case "tagline":
-          let settingData = {
-            id: setting.id,
-            name: setting.name,
-            // value: vm.tagline
-            value: vm[setting.name]
-          };
-
-          if (setting.name) {
-            switch (deploymentTarget) {
-              case LOCALHOST:
-                axios
-                  .put("/v1/settings/" + setting.id, settingData)
-                  .then(function(response) {
-                    console.log(response);
-                  })
-                  .catch(function(error) {
-                    console.log(error);
-                  });
-                break;
-              case FBASE:
-                let db = firebase.firestore();
-                const dbSettings = {
-                  timestampsInSnapshots: true
-                };
-                const moment = require("moment");
-                settingData.updatedAt = moment().format(
-                  "YYYY-MM-DD HH:mm:ss.ms Z"
-                );
-
-                db.settings(dbSettings);
-                db
-                  .collection("settings")
-                  .doc(setting.id)
-                  .update(settingData)
-                  .then(function(docRef) {
-                    console.log("settings saved!");
-                  })
-                  .catch(function(error) {
-                    console.error("Error adding document: ", error);
-                  });
-                break;
-            }
-          }
-          // break;
         }
-
-        // creating a setting if it didn't exist before
-        /*
-            var notThere = true;
-            if(vm.tagline.length){
-                // if the tagline isn't there in vm.settings, create it.
-                for(let i in settings) {
-                    let setting = settings[i];
-                    if(setting.name == 'tagline') notThere = false;
-                }
-                if(notThere) {
-                    // create the new setting if it's not there
-                    let settingData = {
-                          name: 'tagline',
-                            value: vm.tagline
-                      };
-                    axios
-                          .post("/v1/settings", settingData)
-                          .then(function(response) {
-                              console.log(response);
-                          })
-                          .catch(function(error) {
-                              console.log(error);
-                        });
-                }
-            }
-            */
       }
+      // creating a setting if it didn't exist before
+      /*
+              var notThere = true;
+              if(vm.tagline.length){
+                  // if the tagline isn't there in vm.settings, create it.
+                  for(let i in settings) {
+                      let setting = settings[i];
+                      if(setting.name == 'tagline') notThere = false;
+                  }
+                  if(notThere) {
+                      // create the new setting if it's not there
+                      let settingData = {
+                            name: 'tagline',
+                              value: vm.tagline
+                        };
+                      axios
+                            .post("/v1/settings", settingData)
+                            .then(function(response) {
+                                console.log(response);
+                            })
+                            .catch(function(error) {
+                                console.log(error);
+                          });
+                  }
+              }
+              */
     }
   }
 };
