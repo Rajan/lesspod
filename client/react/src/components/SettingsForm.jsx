97d441033792f1c49831932c10dc43f3b7acb90e
s-kris <skris4291@gmail.com>
2018-07-19 14:09:06 -0400

    modified firebase rules && minor fixes for settings

--- client/react/src/components/SettingsForm.jsx
+++ client/react/src/components/SettingsForm.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import CustomLoader from './../components/CustomLoader';
 import { showAlert } from '../utils/utils';
 import { saveSettingsToFbase, uploadLogoToFbase } from '../api/firebase';
 import settingsStore from '../stores/settingsStore';
@@ -56,7 +55,6 @@ class SettingsForm extends React.Component {
     disableNewRegistrations: false,
     squareLogoURL: '',
     horizontalLogoURL: '',
-    isLoading: true,
     isSaving: false,
   };
 
@@ -114,7 +112,6 @@ class SettingsForm extends React.Component {
 
   render() {
     const {
-      isLoading,
       isSaving,
       tagline,
       horizontalLogoURL,
@@ -129,187 +126,181 @@ class SettingsForm extends React.Component {
           <div style={styles.logo} />
           <div style={styles.logoText} />
         </div> */}
-
-        {isLoading ? (
-          <div style={styles.loaderContainer}>
-            <CustomLoader />
+        <div>
+          <div className="field has-text-centered">
+            <span className="icon" style={{ width: '3rem', height: '3rem' }}>
+              <i className="fa fa-cog fas fa-3x" />
+            </span>
+            <br /> Site Settings <br />
           </div>
-        ) : (
-          <div>
-            <div className="field has-text-centered">
-              <span className="icon" style={{ width: '3rem', height: '3rem' }}>
-                <i className="fa fa-cog fas fa-3x" />
+          <br />
+          <div className="field">
+            <label className="label">
+              Square Logo
+              <span style={styles.dimensionsText}>
+                &nbsp;({LOGO_SQUARE_SIDE * 2}px * {LOGO_SQUARE_SIDE * 2}px)
               </span>
-              <br /> Site Settings
-            </div>
-            <div className="field">
-              <label className="label">
-                Square Logo
-                <span style={styles.dimensionsText}>
-                  &nbsp;({LOGO_SQUARE_SIDE * 2}px * {LOGO_SQUARE_SIDE * 2}px)
-                </span>
-                &nbsp; &nbsp; &nbsp;
-                <i
-                  className="fa fa-trash"
-                  style={{ cursor: 'pointer' }}
-                  onClick={() => {
-                    settingsStore.global.squareLogoURL = '';
-                    this.setState({ squareLogoURL: '' });
-                  }}
-                />
-              </label>
-              <div className="control has-icons-left">
-                <div className="file has-name is-boxed">
-                  <label className="file-label">
-                    <input
-                      className="file-input"
-                      type="file"
-                      name="logo"
-                      accept="image/*"
-                      onChange={e => {
-                        this.handleFileUpload(e, 'squareLogoURL');
+              &nbsp; &nbsp; &nbsp;
+              <i
+                className="fa fa-trash"
+                style={{ cursor: 'pointer' }}
+                onClick={() => {
+                  settingsStore.global.squareLogoURL = '';
+                  this.setState({ squareLogoURL: '' });
+                }}
+              />
+            </label>
+            <div className="control has-icons-left">
+              <div className="file has-name is-boxed">
+                <label className="file-label">
+                  <input
+                    className="file-input"
+                    type="file"
+                    name="logo"
+                    accept="image/*"
+                    onChange={e => {
+                      this.handleFileUpload(e, 'squareLogoURL');
+                    }}
+                    style={{ opacity: 0 }}
+                  />
+                  {squareLogoURL.length > 1 ? (
+                    <div
+                      style={{
+                        ...styles.squareLogo,
+                        ...{
+                          backgroundImage: `url(${squareLogoURL})`,
+                        },
                       }}
-                      style={{ opacity: 0 }}
                     />
-                    {squareLogoURL.length > 1 ? (
-                      <div
-                        style={{
-                          ...styles.squareLogo,
-                          ...{
-                            backgroundImage: `url(${squareLogoURL})`,
-                          },
-                        }}
-                      />
-                    ) : (
-                      <span className="file-cta">
-                        <span className="file-icon">
-                          <i className="fa fa-upload" />
-                        </span>
-                        <span className="file-label">Choose a file…</span>
+                  ) : (
+                    <span className="file-cta">
+                      <span className="file-icon">
+                        <i className="fa fa-upload" />
                       </span>
-                    )}
-                  </label>
-                </div>
+                      <span className="file-label">Choose a file…</span>
+                    </span>
+                  )}
+                </label>
               </div>
             </div>
-            <br />
-            <div className="field">
-              <label className="label">
-                Horizontal Logo
-                <span style={styles.dimensionsText}>
-                  &nbsp;({LOGO_HORIZONTAL_WIDTH * 2}px * {LOGO_HORIZONTAL_HEIGHT * 2}px)
-                </span>
-                &nbsp; &nbsp; &nbsp;
-                <i
-                  className="fa fa-trash"
-                  style={{ cursor: 'pointer' }}
-                  onClick={() => {
-                    settingsStore.global.horizontalLogoURL = '';
-                    this.setState({ horizontalLogoURL: '' });
-                  }}
-                />
-              </label>
-              <div className="control has-icons-left">
-                <div className="file has-name is-boxed">
-                  <label className="file-label">
-                    <input
-                      className="file-input"
-                      type="file"
-                      name="logo"
-                      accept="image/*"
-                      onChange={e => {
-                        this.handleFileUpload(e, 'horizontalLogoURL');
+          </div>
+          <br />
+          <div className="field">
+            <label className="label">
+              Horizontal Logo
+              <span style={styles.dimensionsText}>
+                &nbsp;({LOGO_HORIZONTAL_WIDTH * 2}px * {LOGO_HORIZONTAL_HEIGHT * 2}px)
+              </span>
+              &nbsp; &nbsp; &nbsp;
+              <i
+                className="fa fa-trash"
+                style={{ cursor: 'pointer' }}
+                onClick={() => {
+                  settingsStore.global.horizontalLogoURL = '';
+                  this.setState({ horizontalLogoURL: '' });
+                }}
+              />
+            </label>
+            <div className="control has-icons-left">
+              <div className="file has-name is-boxed">
+                <label className="file-label">
+                  <input
+                    className="file-input"
+                    type="file"
+                    name="logo"
+                    accept="image/*"
+                    onChange={e => {
+                      this.handleFileUpload(e, 'horizontalLogoURL');
+                    }}
+                    style={{ opacity: 0 }}
+                  />
+                  {horizontalLogoURL.length > 1 ? (
+                    <div
+                      style={{
+                        ...styles.horizontalLogo,
+                        ...{
+                          backgroundImage: `url(${horizontalLogoURL})`,
+                        },
                       }}
-                      style={{ opacity: 0 }}
                     />
-                    {horizontalLogoURL.length > 1 ? (
-                      <div
-                        style={{
-                          ...styles.horizontalLogo,
-                          ...{
-                            backgroundImage: `url(${horizontalLogoURL})`,
-                          },
-                        }}
-                      />
-                    ) : (
-                      <span className="file-cta">
-                        <span className="file-icon">
-                          <i className="fa fa-upload" />
-                        </span>
-                        <span className="file-label">Choose a file…</span>
+                  ) : (
+                    <span className="file-cta">
+                      <span className="file-icon">
+                        <i className="fa fa-upload" />
                       </span>
-                    )}
-                  </label>
-                </div>
+                      <span className="file-label">Choose a file…</span>
+                    </span>
+                  )}
+                </label>
               </div>
             </div>
-            <div className="field">
-              <label className="label">Tag Line</label>
-              <div className="control has-icons-left">
-                <input
-                  className="input"
-                  type="name"
-                  id="tagline"
-                  name="tagline"
-                  value={tagline}
-                  placeholder="e.g. Serverless Blogging Engine"
-                  onChange={this.handleInputChange}
-                />
-                {/* <span className="icon is-small is-left">
+          </div>
+          <div className="field">
+            <label className="label">Tag Line</label>
+            <div className="control has-icons-left">
+              <input
+                className="input"
+                type="name"
+                id="tagline"
+                name="tagline"
+                value={tagline}
+                placeholder="e.g. Serverless Blogging Engine"
+                onChange={this.handleInputChange}
+              />
+              {/* <span className="icon is-small is-left">
               <i className="fa fa-user-circle" />
             </span> */}
-              </div>
-            </div>
-            <div className="field">
-              <label className="label">Blog Menu</label>
-              <div className="control">
-                <label className="checkbox">
-                  <input
-                    type="checkbox"
-                    name="disableBlogMenu"
-                    checked={disableBlogMenu}
-                    onChange={this.handleInputChange}
-                  />
-                  &nbsp; Disable Blog Menu
-                </label>
-              </div>
-            </div>
-            <div className="field">
-              <label className="label">New Registrations</label>
-              <div className="control">
-                <label className="checkbox">
-                  <input
-                    type="checkbox"
-                    name="disableNewRegistrations"
-                    onChange={this.handleInputChange}
-                    checked={disableNewRegistrations}
-                  />
-                  &nbsp; Disable New Registrations
-                </label>
-              </div>
-            </div>
-            <div className="field is-grouped" style={{ marginTop: '2rem' }}>
-              <div className="control">
-                <div
-                  href="#"
-                  className={`button is-info ${isSaving ? 'is-loading' : ''}`}
-                  onClick={() => {
-                    this.onClickSave();
-                  }}
-                >
-                  Save Settings
-                </div>
-              </div>
-              <div className="control">
-                <Link to="/home">
-                  <div className="button is-text" style={{ textDecoration: 'none', color: '#0271D3' }}>
-                    Cancel
-                  </div>
-                </Link>
-              </div>
             </div>
           </div>
-        )}
+          <div className="field">
+            <label className="label">Blog Menu</label>
+            <div className="control">
+              <label className="checkbox">
+                <input
+                  type="checkbox"
+                  name="disableBlogMenu"
+                  checked={disableBlogMenu}
+                  onChange={this.handleInputChange}
+                />
+                &nbsp; Disable Blog Menu
+              </label>
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">New Registrations</label>
+            <div className="control">
+              <label className="checkbox">
+                <input
+                  type="checkbox"
+                  name="disableNewRegistrations"
+                  onChange={this.handleInputChange}
+                  checked={disableNewRegistrations}
+                />
+                &nbsp; Disable New Registrations
+              </label>
+            </div>
+          </div>
+          <div className="field is-grouped" style={{ marginTop: '2rem' }}>
+            <div className="control">
+              <div
+                href="#"
+                className={`button is-info ${isSaving ? 'is-loading' : ''}`}
+                onClick={() => {
+                  this.onClickSave();
+                }}
+              >
+                Save Settings
+              </div>
+            </div>
+            <div className="control">
+              <Link to="/home">
+                <div className="button is-text" style={{ textDecoration: 'none', color: '#0271D3' }}>
+                  Cancel
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
